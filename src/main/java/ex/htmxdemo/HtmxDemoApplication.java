bfb12f7420984df0c693fa613579620509464229
yunjae62 <wodbs7758@naver.com>
2026-08-05 21:34:49 +0900

    first commit

---
+++ src/main/java/ex/htmxdemo/HtmxDemoApplication.java
@@ -0,0 +1,13 @@
+package ex.htmxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HtmxDemoApplication.class, args);
+    }
+
+}
